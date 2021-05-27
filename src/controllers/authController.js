f0e0bae160ec68f9943ebbed36f2acde3c247038
Matheus Figueiredo <ultra10vascaino@gmail.com>
2021-05-27 18:57:39 -0300

    Adicionando módulos.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
-var bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router()
 const User = require('../models/usuario')
+const config = require("../config/auth")
 
 /* Autenticação */
 
