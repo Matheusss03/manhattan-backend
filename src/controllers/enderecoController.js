d9165862cd67ba3dee81b71504efdd0aebba66bf
Matheus Figueiredo <ultra10vascaino@gmail.com>
2021-06-10 17:24:29 -0300

    Atualizando API de endereços

--- src/controllers/enderecoController.js
+++ src/controllers/enderecoController.js
@@ -5,15 +5,20 @@ const Endereco = require('../models/endereco')
 const router = express.Router()
 
 
-// Lista todas os endereços
-router.get('/todos', async (req, res) =>{
-    try {
-        const enderecos = await Endereco.find()
+/* Listar Todos Diarios*/
+router.get('/todos', async (req, res) => {
+    await Endereco.find(function(err,endereco){
+        if(err) res.status(400).send({error: 'Erro ao pegar todos os endereços!!  ' + err})
+        else res.json(endereco)
+    })
 
-        return res.send({ enderecos })
-    } catch (err) {
-        return res.status(400).send({ error:'Erro ao pegar todos endereços!!  ' + err })
-    }
+})
+
+/* Pega um endereço específico */
+router.get('/:id', async (req, res) => {
+    await Endereco.findById(req.params.id)
+    .then(endereco => res.json(endereco))
+    .catch(err => res.status(404).json({datafound: 'Dado não encontrado'}))
 })
 
 // Cadastra um endereço
@@ -27,15 +32,22 @@ router.post('/add', async (req, res) =>{
     }
 })
 
-// Remove um endereço
-router.delete('/:id', async (req, res) => {
+/* Deleta um endereço */
+router.delete('/delete/:id', async (req, res) => {
     try {
         await Endereco.findByIdAndRemove(req.params.id)
 
         return res.send()
     } catch (err) {
-        return res.status(400).send({ error:'Erro ao apagar um endereço!!  ' + err })
+        return res.status(400).send({error: 'Erro ao deletar objeto!  ' + err})
     }
+})
+
+/* Atualiza Endereço */
+router.put('/update/:id', async (req, res) => {
+    await Endereco.findByIdAndUpdate(req.params.id, req.body)
+    .then(endereco => res.json(endereco))
+    .catch(err => res.status(400).json({ error: 'Erro ao atualizar o banco' }))
 })
 
 module.exports = app => app.use('/endereco', router)
