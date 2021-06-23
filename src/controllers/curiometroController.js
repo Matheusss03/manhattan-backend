72e6cd748e8f3b4f7e154fbfa0b1dff31e9f55e0
Matheus Figueiredo <ultra10vascaino@gmail.com>
2021-06-23 03:02:15 -0300

    Atualizacao de controllers.

--- src/controllers/curiometroController.js
+++ src/controllers/curiometroController.js
@@ -16,7 +16,7 @@ router.get('/todos', async (req, res) => {
 /* Novo Registro */
 router.post('/add', async (req, res) => {
     try {
-        const diario = await DadoCalibrador.create({ ...req.body, usuario: req.userId })
+        const diario = await DadoCalibrador.create({ ...req.body, usuario: req.usuarioId })
 
         return res.send( { diario })
     } catch (err) {
